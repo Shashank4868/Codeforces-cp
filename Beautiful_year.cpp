7e510232a4f590ade4b7b62f0253b90518ccc0e3
Shashank <2006043@kiit.ac.in>
2023-09-14 01:39:26 +0530

    solved

--- Beautiful_year.cpp
+++ Beautiful_year.cpp
@@ -1,45 +1,71 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define vec vector<ll>
 #define pb push_back
-#define fl(j,n) for(ll i = j;i < n ; i++)
+#define fl(j, n) for (ll i = j; i < n; i++)
 
-void fast_io(){
+void fast_io()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 }
 
-void solve() {
+void solve()
+{
     int yr;
-    cin>>yr;
-    while(++yr) {
-        string s = to_string(yr);
-        bool x = true;
-        unordered_map<char, int> hash;
-        for(auto it : s) {
-            hash[it]++;
-            if(hash[it] > 1) {
-                x = false;
+    cin >> yr;
+    while (++yr)
+    {
+        int x = yr;
+        unordered_set<int> hash;
+        bool flag = false;
+        while (x) // 1987 -> 1 9 8 9
+        {
+            int a = x % 10;    // 9 8 9 1
+            if (hash.count(a)) // duplicate
+            {
+                flag = true;
                 break;
             }
+            hash.insert(a); // hash =>   9, 8
+            x /= 10;        // 198
         }
-        if(x) {
-            cout<<yr;
+
+        if (!flag) // if(flag == false)
+        {
+            cout << yr;
             break;
         }
+
+        // string s = to_string(yr);
+        // bool x = true;
+        // unordered_map<char, int> hash;
+        // for(auto it : s) {
+        //     hash[it]++;
+        //     if(hash[it] > 1) {
+        //         x = false;
+        //         break;
+        //     }
+        // }
+        // if(x) {
+        //     cout<<yr;
+        //     break;
+        // }
     }
 }
 
-int main(){
+int main()
+{
     fast_io();
-    ll test_cases = 1;;
+    ll test_cases = 1;
+    ;
     // cin>>test_cases;
     while (test_cases--)
     {
         solve();
     }
-    
+
     return 0;
 }
